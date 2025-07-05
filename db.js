a1c31daf35a6430d6d8cc8c77131fd52caf19d5e
JosuedavidRH <jrh24555@gmail.com>
2025-07-05 15:37:02 -0500

    configuracion a render

--- db.js
+++ db.js
@@ -1,16 +1,18 @@
-// backend/db.js
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',  // reemplaza con tu contraseña real
-  database: 'testdb'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
-  if (err) throw err;
-  console.log('Conectado a la base de datos MySQL');
+  if (err) {
+    console.error('Error de conexión: ' + err.stack);
+    return;
+  }
+  console.log('Conectado a la base de datos');
 });
 
 module.exports = connection;
