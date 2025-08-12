6fe0d176b6ae42bc523a31f8a53758cb9469e9c0
JosuedavidRH <jrh24555@gmail.com>
2025-08-12 17:31:52 -0500

    v_16

--- realTime.js
+++ realTime.js
@@ -80,7 +80,7 @@ router.post('/temporizador', (req, res) => {
 
 
 // Guardar  el statusActual
-app.post('/api/statusActual', (req, res) => {
+router.post('/api/statusActual', (req, res) => {
   const { userId, statusActual } = req.body;
 
   if (!userId || statusActual === undefined) {
